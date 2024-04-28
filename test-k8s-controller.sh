446f8806dbb2d85d45a9b3918224cde9e88ba211
phi99 <34248917+phi99@users.noreply.github.com>
2024-04-28 12:25:56 +1000

    Update and rename test-controller.sh to test-k8s-controller.sh

--- test-k8s-controller.sh
+++ test-k8s-controller.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 watch_event() {
-  # Watch the K8s API on events on service objects
-  echo "::: Starting to wait for events"
-
-  # Event loop listening for changes in config maps
-  curl -N -H "Authorization: Bearer $TOKEN" -s https://10.96.0.1/api/v1/namespaces/test-namespace/configmaps?watch=true | while read -r event
-  #curl -N -s $base/api/v1/${ns}/configmaps?watch=true | while read -r event
+  # Listening to K8s API on events on configmap resource
+  echo "- Listening to K8s events -"
+  curl -N -H "Authorization: Bearer $TOKEN" -s https://<API server ip address>/api/v1/namespaces/<target namespace>/configmaps?watch=true | while read -r event
   do
-    # Sanitize new lines
     event=$(echo "$event" | tr '\r\n' ' ')
     echo "event: $event"
     echo
-    # Event type & name
+    # Checking event type & name
     local type=$(echo "$event" | jq -r .type)
     local config_map=$(echo "$event" | jq -r .object.metadata.name)
 
@@ -36,6 +32,7 @@ watch_event() {
 }
 
 get_pods() {
+  # Retrieve pods with target label
   local selector=${1}
   echo "selector:$selector"
   pods=$(curl -N -H "Authorization: Bearer $TOKEN" -s https://10.96.0.1/api/v1/namespaces/test-namespace/pods?labelSelector=$selector | jq -r .items[].metadata.name)
@@ -44,7 +41,7 @@ get_pods() {
 }
 
 delete_pods() {
-  # Delete all pods that matched
+  # Delete pods that matched
   for pod in $pods; do
     # Delete but also check exit code
     exit_code=$(curl -N -H "Authorization: Bearer $TOKEN" -X DELETE -o /dev/null -w "%{http_code}" -s  https://10.96.0.1/api/v1/namespaces/test-namespace/pods/$pod)
